667891f63b62a63723ced84c231697ebedfc2a7e
LongQue <1220756253@qq.com>
2020-07-20 23:01:01 +0800

    定义分发模块关于模板及计算模块的Fegin接口

---
+++ imooc-coupon-service/coupon-distribution/src/main/java/com/imooc/coupon/feign/SettlementClient.java
@@ -0,0 +1,25 @@
+package com.imooc.coupon.feign;
+
+import com.imooc.coupon.exception.CouponException;
+import com.imooc.coupon.vo.CommonResponse;
+import com.imooc.coupon.vo.SettlementInfo;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * <h1>优惠券结算Feign接口定义</h1>
+ *
+ * @author ZhouFufeng
+ * @since 2020/7/20
+ */
+@FeignClient(value = "eureka-client-coupon-settlement")
+public interface SettlementClient {
+
+    /**
+     * <h2>优惠券规则计算</h2>
+     */
+    @RequestMapping(value = "/coupon-settlement/settlement/compute",method = RequestMethod.POST)
+    CommonResponse<SettlementInfo> computeRule(@RequestBody SettlementInfo settlement) throws CouponException;
+}
